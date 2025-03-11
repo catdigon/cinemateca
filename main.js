8997997481621baec027fd15fc91a6a7530a75a8
catdigon <catiagon1@hotmail.com>
2025-03-11 00:39:28 +0000

    Filters accordion

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function hideElementAlways(selector) {
 
 function handleHideHamburguerWhenScreenLarge() {
   var navbarHamburguer = document.querySelector(".navbar-hamburguer-container");
-  if (window.innerWidth > 768) {
+  if (window.innerWidth > 768 && navbarHamburguer) {
     navbarHamburguer.style.display = "none";
   }
 }
@@ -27,3 +27,31 @@ function handleHideHamburguerWhenScreenLarge() {
 document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener('resize', handleHideHamburguerWhenScreenLarge);
 });
+
+var acordionFilters = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acordionFilters.length; i++) {
+  acordionFilters[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    //update the aria-expanded when closing
+    this.setAttribute("aria-expanded", "false");
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      //update the aria-expanded when opening
+      this.setAttribute("aria-expanded", "true");
+    } 
+  });
+}
+
+var filterOptions = document.getElementsByClassName('filter-option');
+
+for (i = 0; i < filterOptions.length; i++) {
+    filterOptions[i].addEventListener('click', function() {
+        // Alterna a classe 'show-icon' para mostrar ou esconder o ícone
+        this.classList.toggle('add-filter');
+    });
+}
