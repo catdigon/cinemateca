d31979e98b865bc9170b9037582b05ae6009be5b
catdigon <catiagon1@hotmail.com>
2025-04-13 20:28:06 +0100

    Start implementing the filter modal button for small screens.

--- js/filters.js
+++ js/filters.js
@@ -1,3 +1,13 @@
-//**Search for the filter navbar*/
-const content = document.getElementById("filters-modal").innerHTML;
-document.getElementById("filters-mobile").innerHTML = content;
+function mover() {
+    const div = document.getElementById("filter-toolbar");
+    const destino = document.getElementById("filtersMobile");
+    destino.appendChild(div);
+  }
+
+
+  function reverter() {
+    const div = document.getElementById("filter-toolbar");
+    const destinoOriginal = document.getElementById("filterDesktop");
+    const mainResults = document.getElementById("main-container")
+    destinoOriginal.insertBefore(div, mainResults)
+  }
